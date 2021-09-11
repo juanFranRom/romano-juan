20c91b6da428f0727902515c66e5f2cdee9c317e
Juan Romano <romanoj.francisco@gmail.com>
2021-09-10 23:55:59 -0300

    Feature Delay in Promise

--- src/Components/ItemListContainer/index.js
+++ src/Components/ItemListContainer/index.js
@@ -1,12 +1,38 @@
+import { useState, useEffect } from 'react'
 import { List } from './Elements'
 import { data } from './data'
 import { ItemList } from '../ItemList'
 
+const getProductos = new Promise((res, rej) => {
+    const status = 200;
+    setTimeout(() => {
+        status === 200 ? res(data) : rej("error")
+    }, 2000)
+})
+
 export default function ItemListContainer() {
+    const [productos, setProductos] = useState([])
+    const [loading, setLoading] = useState(true)
+
+    useEffect(() => {
+        getProductos
+        .then(result => {
+            setProductos(result)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+        .finally(() => {
+            setLoading(!loading)
+        })
+    }, [])
+
     return (
         <List>
             <h1>Productos</h1>
-            <ItemList items={data} />
+            {
+                loading ? <h2>Cargando</h2> : <ItemList items={data} />
+            }
         </List>
     )
 }
