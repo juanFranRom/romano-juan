ccfb703519eac9c074e7f3e612cf1425aa082d1e
juanFranRom <89553596+juanFranRom@users.noreply.github.com>
2021-10-13 19:22:45 -0300

    Feature-ShowTicket

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -15,14 +15,17 @@ import {
 } from './elements'
 import { useCartContext } from '../../context/CartContext'
 import Button from '../Button'
+import Loading from '../Loading'
 import { Form } from '../Form'
 import { createOrder } from '../../helpers/firebase'
 
 
 export const Cart = () => {
+    const [idTicket, setIdTicket] = useState(null)
+    const [loadingTicket, setLoadingTicket] = useState(false)
     const [isShopping, setIsShopping] = useState(true)
     const [user, setUser] = useState({})
-    const { cart, removeItem, cartLength, cartTotal } = useCartContext()
+    const { cart, removeItem, cartLength, cartTotal, clear } = useCartContext()
     
     const handleUser = event => {
         setUser({
@@ -55,9 +58,14 @@ export const Cart = () => {
             total: cartTotal()
         }
         
+        setLoadingTicket(true)
+
         createOrder(order)
-        .then(data => console.log(data.id))
-        .finally(() => window.location.replace("/"))
+        .then(data => {
+            setIdTicket(data.id)
+            console.log(data.id)
+        })
+        .finally(() => clear())
     }
 
     let lenCart = cartLength()
@@ -65,64 +73,88 @@ export const Cart = () => {
     console.log(user);
     return (
         <CartContainer>
+            <CartDisplay>
             {
-                <CartDisplay>
+                !loadingTicket
+                ?
+                    <>
+                        {
+                            isShopping
+                            ?
+                                lenCart > 0 
+                                ? 
+                                    <>
+                                        <CartItem>
+                                            <h1>Carrito ({lenCart})</h1>
+                                        </CartItem>
+                                            {
+                                                cart.map(({item, quantity}, index) => {
+                                                    return(
+                                                        <CartItem key={index}>
+                                                            <Item>
+                                                                <img src={`${process.env.PUBLIC_URL}/assets/Images/${item.image}`} alt={item.name}/>
+                                                                <div>
+                                                                    <h3>{item.name}</h3>
+                                                                    <p>{`${quantity} Kg`}</p>
+                                                                </div>
+                                                            </Item>
+                                                            <Precio>
+                                                                <Eliminar onClick={() => handleRemove(item.id)}/>
+                                                                {`${item.price * quantity} $`}
+                                                            </Precio>
+                                                        </CartItem>
+                                                    )
+                                                })
+                                            }
+                                        <SubmitItem>
+                                            <SubmitContainer>
+                                                <Precio>
+                                                    {`${cartTotal()} $`}
+                                                </Precio>
+                                                <Button text={'Finalizar Compra'} onAdd={()=>setIsShopping(false)}/>
+                                            </SubmitContainer>
+                                        </SubmitItem>
+                                    </>
+                                :
+                                    <EmptyCart>
+                                        <div>
+                                            <h1>¡Tu carrito esta vacio!</h1>
+                                            <p>¡Miles de productos te esperan!</p>
+                                        </div>
+                                        <ButtonContainer>
+                                            <ButtonLink to='/'>
+                                                <Button text={"Segui Comprando"}/>
+                                            </ButtonLink>
+                                        </ButtonContainer>
+                                    </EmptyCart>
+                            :   
+                                <FormDisplay>
+                                    <Form handleUser={handleUser} handleIsShopping={handleIsShopping} endShopping={handleOrder}/>
+                                </FormDisplay>
+                        }
+                    </>
+                :
+                    <>
                     {
-                        isShopping
+                        !idTicket
                         ?
-                            lenCart > 0 
-                            ? 
-                                <>
-                                    <CartItem>
-                                        <h1>Carrito ({lenCart})</h1>
-                                    </CartItem>
-                                        {
-                                            cart.map(({item, quantity}, index) => {
-                                                return(
-                                                    <CartItem key={index}>
-                                                        <Item>
-                                                            <img src={`${process.env.PUBLIC_URL}/assets/Images/${item.image}`} alt={item.name}/>
-                                                            <div>
-                                                                <h3>{item.name}</h3>
-                                                                <p>{`${quantity} Kg`}</p>
-                                                            </div>
-                                                        </Item>
-                                                        <Precio>
-                                                            <Eliminar onClick={() => handleRemove(item.id)}/>
-                                                            {`${item.price * quantity} $`}
-                                                        </Precio>
-                                                    </CartItem>
-                                                )
-                                            })
-                                        }
-                                    <SubmitItem>
-                                        <SubmitContainer>
-                                            <Precio>
-                                                {`${cartTotal()} $`}
-                                            </Precio>
-                                            <Button text={'Finalizar Compra'} onAdd={()=>setIsShopping(false)}/>
-                                        </SubmitContainer>
-                                    </SubmitItem>
-                                </>
-                            :
-                                <EmptyCart>
-                                    <div>
-                                        <h1>¡Tu carrito esta vacio!</h1>
-                                        <p>¡Miles de productos te esperan!</p>
-                                    </div>
-                                    <ButtonContainer>
-                                        <ButtonLink to='/'>
-                                            <Button text={"Segui Comprando"}/>
-                                        </ButtonLink>
-                                    </ButtonContainer>
-                                </EmptyCart>
-                        :   
-                            <FormDisplay>
-                                <Form handleUser={handleUser} handleIsShopping={handleIsShopping} endShopping={handleOrder}/>
-                            </FormDisplay>
+                            <Loading color={'rgb(139,39,39)'}/>
+                        :
+                        <EmptyCart>
+                            <div>
+                                <h1>¡Ya ha finalizado de procesarse tu compra!</h1>
+                                <p>Podras tener un seguimiento de la misma con tu ticket "{idTicket}"</p>
+                            </div>
+                            <ButtonContainer>
+                                <ButtonLink to='/'>
+                                    <Button text={"Volver al Inicio"}/>
+                                </ButtonLink>
+                            </ButtonContainer>
+                        </EmptyCart>
                     }
-                </CartDisplay>
+                    </>
             }
+            </CartDisplay>
         </CartContainer>
     )
 }
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -11,30 +11,30 @@ export const Form = ({ handleUser, handleIsShopping, endShopping }) => {
 
     return (
         <>
-        <FormWrapper action='/' onChange={handleUser} onSubmit={endShopping}>
-            <h1>!Comparti tus datos con nosotros!</h1>
-            <h2>Asi podremos garantizar la seguridad de tu pedido</h2>
-            <InputWrapper>
-                Nombre:
-                <input type='text' name='name' required/>
-            </InputWrapper>
-            <InputWrapper>
-                Telefono:
-                <input type='number' name='tel' required/>
-            </InputWrapper>
-            <InputWrapper>
-                Email:
-                <input type='email' name='email' required/>
-            </InputWrapper>
-            <ButtonsWrapper>
-                <CancelWrapper>
-                    <Button type={'none'} text='Seguir Comprando' color={'red'} onAdd={handleIsShopping}/>
-                </CancelWrapper>
-                <SubmitWrapper>
-                    <Button type={'submit'} text='Finalizar Compra'/>
-                </SubmitWrapper>
-            </ButtonsWrapper>
-        </FormWrapper>
-    </>
+            <FormWrapper onChange={handleUser} onSubmit={endShopping}>
+                <h1>!Comparti tus datos con nosotros!</h1>
+                <h2>Asi podremos garantizar la seguridad de tu pedido</h2>
+                <InputWrapper>
+                    Nombre:
+                    <input type='text' name='name' required/>
+                </InputWrapper>
+                <InputWrapper>
+                    Telefono:
+                    <input type='number' name='tel' required/>
+                </InputWrapper>
+                <InputWrapper>
+                    Email:
+                    <input type='email' name='email' required/>
+                </InputWrapper>
+                <ButtonsWrapper>
+                    <CancelWrapper>
+                        <Button type={'none'} text='Seguir Comprando' color={'red'} onAdd={handleIsShopping}/>
+                    </CancelWrapper>
+                    <SubmitWrapper>
+                        <Button type={'submit'} text='Finalizar Compra'/>
+                    </SubmitWrapper>
+                </ButtonsWrapper>
+            </FormWrapper>
+        </>
     )
 }
